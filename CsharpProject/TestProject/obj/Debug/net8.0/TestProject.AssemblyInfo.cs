bdfe583ec30d29c9912c5bb47f1d8ca11b687da3
Pedro Luiz de Souza <pedroelucassouza@gmail.com>
2024-06-27 21:48:55 -0300

    Métodos sobrecarregados

--- CsharpProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProject/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa519c83aabd39c620542ae25f007f05b5455a8e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
